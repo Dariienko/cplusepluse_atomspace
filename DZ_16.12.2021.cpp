ef794380126cb5d4daef0bd2bcd47bde040f84ce
Dariienko Dmytro <dariienko.dima@gmail.com>
2021-12-17 04:04:33 +0200

    Create DZ_16.12.2021.cpp

---
+++ DZ_16.12.2021.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main() {
+    int value = 15;
+    int a[] = { 1, 3, 4, 12, 15, 16 };
+    int counter = 0;
+    int low = 0;
+    int mid = 0;
+    int high = sizeof(a) / sizeof(int) - 1;
+    while (low != high && a[mid] != value)
+    {
+        mid = (low + high) / 2;
+        if (a[mid] < value) 
+        {
+            low = mid;
+        } 
+        else high = mid;
+        counter++;
+    }
+    std::cout << "value " << a[mid] << " found "<< std::endl <<"index = "<< mid << std::endl <<"steps done = "<< counter;
+}
