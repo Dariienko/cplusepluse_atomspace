3a889f7f7fe6c3d89d60eb12229a1a03d4ccef47
Dariienko Dmytro <dariienko.dima@gmail.com>
2021-12-17 16:46:33 +0200

    Update DZ_16.12.2021.cpp

--- DZ_16.12.2021.cpp
+++ DZ_16.12.2021.cpp
@@ -1,21 +1,26 @@
 ﻿#include <iostream>
 
-int main() {
-    int value = 15;
-    int a[] = { 1, 3, 4, 12, 15, 16 };
-    int counter = 0;
-    int low = 0;
-    int mid = 0;
-    int high = sizeof(a) / sizeof(int) - 1;
-    while (low != high && a[mid] != value)
+int main()
+{
+    int value = 2;
+    int a[] = {-1, 3, 4, 7, 10, 11, 12, 15, 16, 29};
+    int counter = 0, left = 0, mid = 0;
+    int right = sizeof(a) / sizeof(int) - 1;
+    while (left <= right)
     {
-        mid = (low + high) / 2;
-        if (a[mid] < value) 
+        mid = (left + right) / 2;
+        if (a[mid] == value)
         {
-            low = mid;
-        } 
-        else high = mid;
+            std::cout << "value = " << a[mid] << " found" << std::endl << "index = " << mid << std::endl << "steps done = " << counter;
+            return 0;
+        }
+        else if (a[mid] < value)
+        {
+            left = mid + 1;
+        }
+        else
+            right = mid - 1;
         counter++;
     }
-    std::cout << "value " << a[mid] << " found "<< std::endl <<"index = "<< mid << std::endl <<"steps done = "<< counter;
+    std::cout << "value not found" << std::endl << "steps done = " << counter;
 }
