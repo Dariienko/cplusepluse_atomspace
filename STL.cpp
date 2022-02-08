25470401de8cfe37a92c7e9291cf097347a33099
Dariienko Dmytro <dariienko.dima@gmail.com>
2022-02-08 16:55:00 +0200

    update

---
+++ STL.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <stack>
+#include <map>
+#include <list>
+#include <string>
+
+int main()
+{
+    std::map<int,std::string> a;
+}
